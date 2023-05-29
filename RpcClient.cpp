46f5154d7d58187cdc0646ff01618176b9887055
wei-tsh <1301209713@qq.com>
2023-05-29 13:01:29 +0800

    first commit

---
+++ RpcClient.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+
+int main(int argc, char const *argv[])
+{
+    std::cout<<"111";
+    return 0;
+}
---
+++ RpcServer.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+
+int main(int argc, char const *argv[])
+{
+    std::cout<<"222";
+    return 0;
+}
